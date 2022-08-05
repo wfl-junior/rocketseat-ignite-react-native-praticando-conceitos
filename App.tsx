4bd821067e9d4e24b174f814f6d3cb24908e7730
Wallace Júnior <wflj1997@hotmail.com>
2022-08-05 13:14:45 -0300

    setup font

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
-import { Fragment } from "react";
+import { Inter_400Regular, Inter_700Bold } from "@expo-google-fonts/inter";
+import * as Font from "expo-font";
+import * as SplashScreen from "expo-splash-screen";
+import { Fragment, useCallback, useEffect, useState } from "react";
 import { StatusBar, StyleSheet, Text, View } from "react-native";
 
 const styles = StyleSheet.create({
@@ -10,23 +13,56 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 36,
-    fontWeight: "700",
     color: "white",
+    fontFamily: "Inter_700Bold",
   },
 });
 
-const App: React.FC = () => (
-  <Fragment>
-    <StatusBar
-      barStyle="light-content"
-      backgroundColor="transparent"
-      translucent
-    />
+const App: React.FC = () => {
+  const [appIsReady, setAppIsReady] = useState(false);
 
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello World</Text>
-    </View>
-  </Fragment>
-);
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+
+        await Font.loadAsync({
+          Inter_400Regular,
+          Inter_700Bold,
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
+  return (
+    <Fragment>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+
+      <View style={styles.container} onLayout={onLayoutRootView}>
+        <Text style={styles.title}>Hello World</Text>
+      </View>
+    </Fragment>
+  );
+};
 
 export default App;
